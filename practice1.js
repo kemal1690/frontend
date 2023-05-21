bba87defd0704c356575f7eede39683634aaafa3
kemal1690 <kemalaslan169083@gmail.com>
2023-05-22 00:21:24 +0300

    html and css practice

---
+++ practice1.js
@@ -0,0 +1,39 @@
+//One line comment
+
+/*
+multi line comment
+*/
+console.log('Hello World');//shortcut: clg
+console.log("HEllo World with double quote");
+//Js is not a strongly typed language
+//Js is a CASE SENSİTİVE language
+
+//3 ways we can define variable
+
+//var,let 
+//const if the variable is fixed and can not be changed
+//let: you can declare the variable inside the block
+//var: you can declare it whereever you want
+
+let message = "Hello to Js";
+console.log(message);
+
+var num = 10;//we can also create with let
+
+console.log(num);
+
+let price = 1399.99;
+
+console.log('Iphone price is '+ price);
+
+let hired = true;
+
+console.log('Is Ali hired '+ hired);
+
+let coffee;
+
+console.log(coffee);//undefined
+
+let negativity = null;
+
+console.log(negativity);
