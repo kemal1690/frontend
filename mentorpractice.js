439477ed74f7d1615ef62bf3d5480f73002912c7
kemal1690 <kemalaslan169083@gmail.com>
2023-05-24 18:35:04 +0300

    Mentor session practice

--- mentorpractice.js
+++ mentorpractice.js
@@ -1,16 +1,24 @@
 let num1 = 10;
 let num2 = 20;
+let name = 'Ali';
 
-let name = 'Ali'
 
-/*
-let addNumbers = (num1, num2) => {
-         return num1 + num2; 
+
+//let addNumbers = (num1, num2) => {
+//         return num1 + num2; 
+//}
+
+
+//console.log(addNumbers(num1,num2));
+
+//let addNumbers1 = (num1, num2) => num1 + num2; 
+
+function addNumbers(num1, num2) {
+    return num1 + num2; 
 }
-*/
-let addNumbers1 = (num1, num2) => num1 + num2; 
 
-console.log(addNumbers1(num1,num2));
+
+console.log(addNumbers(num1,num2));
 //console.log(typeof 'Ali');
 //console.log(typeof 10);
 
